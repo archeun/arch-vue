89cd4f7ae6236eb5c76e562bb960523d7c7c5752
arunatebel <arunautebel@gmail.com>
2021-08-16 00:36:00 +0530

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <HelloArchVue msg="Welcome to arch-vue!" />
+</template>
+
+<script setup>
+import HelloArchVue from './components/HelloArchVue.vue';
+
+</script>
---
+++ src/components/HelloArchVue.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  msg: { type: String, default: '' },
+});
+
+</script>
