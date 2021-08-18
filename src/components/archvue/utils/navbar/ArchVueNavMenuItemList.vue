a05a8e55e262d852cc042f18e0a53a9b8cad2b05
arunatebel <arunautebel@gmail.com>
2021-08-18 23:46:09 +0530

    formatted css class names

--- src/components/archvue/utils/navbar/ArchVueNavMenuItemList.vue
+++ src/components/archvue/utils/navbar/ArchVueNavMenuItemList.vue
@@ -43,21 +43,19 @@ export default {
   @apply bg-archTeal text-white px-3 py-2 rounded-md text-base font-medium;
 }
 .inactive-main-menu-item {
-  @apply text-archTeal hover:bg-archTeal
-  hover:text-white px-3 py-2 rounded-md text-base font-medium;
+  @apply text-archTeal hover:bg-archTeal hover:text-white
+           px-3 py-2 rounded-md text-base font-medium;
 }
 .active-mobile-menu-item {
   @apply bg-archTeal text-white block px-3 py-2 rounded-md text-base font-medium;
 }
 .inactive-mobile-menu-item {
   @apply text-archTeal hover:bg-archTeal
-  hover:text-white block px-3 py-2 rounded-md text-base font-medium;
+         hover:text-white block px-3 py-2 rounded-md text-base font-medium;
 }
 .mobile-menu-hamburger {
-  @apply bg-gray-800 inline-flex
-            items-center justify-center p-2 rounded-md
-            text-gray-400 hover:text-white hover:bg-gray-700
-            focus:outline-none focus:ring-2 focus:ring-offset-2
-            focus:ring-offset-gray-800 focus:ring-white;
+  @apply bg-gray-800 inline-flex items-center justify-center p-2 rounded-md
+         text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none
+           focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white;
 }
 </style>
